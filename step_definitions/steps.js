caf48d0dd66383457967d00e420a8ffa737e0b5d
Alberto Narda <albertonarda@gmail.com>
2018-10-19 17:08:03 +0200

    fix

--- step_definitions/steps.js
+++ step_definitions/steps.js
@@ -9,18 +9,32 @@ When("I click on I Feel Lucky button", function() {
   client.click("input[name='btnI']");
 });
 
-Then("the url should be {string}", function(string) {
-  return client.assert.urlEquals(string);
+Then(/^the Google search form exists$/, () => {
+  return client.assert.visible('input[name="q"]');
 });
+
+/*GENERIC STEPS*/
 
 Given("I open the url {string}", function(string) {
   return client.url(string);
 });
 
-Then(/^the title should be "(.*?)"$/, text => {
-  return client.assert.title(text);
+When("I click on the element {string}", function(string) {
+  return client.click(string);
 });
 
-Then(/^the Google search form exists$/, () => {
-  return client.assert.visible('input[name="q"]');
+Then("the url should be {string}", function(string) {
+  return client.assert.urlEquals(string);
+});
+
+Then("the title should be {string}", function(string) {
+  return client.assert.title(string);
+});
+
+Then("the element {string} should contain {string}", function(string, text) {
+  return client.expect.element(string).text.to.contain(text);
+});
+
+Then("the element {string} should be visible", function(string) {
+  return client.assert.visible(string);
 });
